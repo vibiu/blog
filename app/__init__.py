2628edc774d5c2d00b97e9da481bf8b9e624d21a
vibiu <vibiu_ubuntu@163.com>
2016-04-27 20:48:24 +0800

    debug1

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 from config import config
 
-db = SQLALchey()
+db = SQLAlchey()
 
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config[confi_name])
+    app.config.from_object(config[config_name])
 
     config[config_name].init_app(app)
     db.init_appp(app)
