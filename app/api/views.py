a6a3f7cee50cf96b8d17d1d9831bf8a5017d8156
vibiu <vibiu_ubuntu@163.com>
2016-05-14 01:43:52 +0800

    cros

--- app/api/views.py
+++ app/api/views.py
@@ -14,8 +14,8 @@ def allow_cross_domain(fun):
     def wrapper_fun(*args, **kwargs):
         rst = make_response(fun(*args, **kwargs))
         rst.headers['Access-Control-Allow-Origin'] = '*'
-        rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
-        allow_headers = 'Referer,Accept,Origin,User-Agent'
+        rst.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+        allow_headers = 'Referer,Accept,Origin,User-Agent,Content-Type, X-Requested-With'
         rst.headers['Access-Control-Allow-Headers'] = allow_headers
         return rst
     return wrapper_fun
