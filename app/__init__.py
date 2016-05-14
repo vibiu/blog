1f34b8d015669efec511c476d1f890010be6de1c
vibiu <vibiu_ubuntu@163.com>
2016-05-14 12:43:25 +0800

    try to cors but give e up

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,7 @@ from flask import Flask,current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.markdown import Markdown
 from config import config
-
 db = SQLAlchemy()
-
 
 def create_app(config_name):
     app = Flask(__name__)
